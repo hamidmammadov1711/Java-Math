ced1a4cea27093b2c603a34516e51aceb5fff709
hamidmammadov1711 <104730137+hamidmammadov1711@users.noreply.github.com>
2022-11-11 18:55:58 +0400

    Add files via upload

---
+++ src/riyazihesablama/RiyaziHesab.java
@@ -0,0 +1,31 @@
+
+package riyazihesablama;
+
+public class RiyaziHesab {
+    public static void main(String[] args) {
+        
+        double x=4;
+        double y=5;
+            System.out.println("x = "+x+" (Double Data Type)");
+            System.out.println("y = "+y+" (Double Data Type)");
+            System.out.println("---------------------------------------------");
+        
+            System.out.println("x ve y Deyerlerinin En Boyuk Olani: "+ Math.max(x, y));
+            System.out.println("x ve y Deyerlerinden En Kicik Olani:" + Math.min(x, y)); 
+            System.out.println("---------------------------------------------");
+           
+        double pi = Math.PI;
+            System.out.println("'pi' Deyiseninin PI Deyeri: " +pi);    
+            System.out.println("---------------------------------------------");
+         
+            System.out.println("'x' Deyerinin Kvardat Koku: " +Math.sqrt(x));
+            System.out.println("---------------------------------------------------");
+         
+            System.out.println("'x'-i 'y' Deyeri Defe Quvvete Yukseltmesi: " +Math.pow(x, y) );
+            
+            
+            System.out.println("Bizi Izlediyiniz Ucun Tewekkurler....");
+            
+    }
+    
+}
